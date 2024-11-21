6551f1f8b02f19aa0ddc455717e379bdcbfafa9a
jeclass <jeclass@outlook.com>
2024-11-21 22:19:02 +0530

    Update Whyneed.jsx

--- src/components/Whyneed.jsx
+++ src/components/Whyneed.jsx
@@ -20,7 +20,7 @@ const Whyneed = () => {
         </div>
       </div>
 
-      <div className="sm:w-5/6 w-[98%] md:w-[98%] sm:flex-row flex-col sm:h-96 container mx-auto sm:mt-20 mt-10 backdrop-blur-lg  bg-gradient-to-b from-gray-100/20 to-transparent xl:p-16 sm:p-8 p-4 flex items-center justify-between">
+{/*       <div className="sm:w-5/6 w-[98%] md:w-[98%] sm:flex-row flex-col sm:h-96 container mx-auto sm:mt-20 mt-10 backdrop-blur-lg  bg-gradient-to-b from-gray-100/20 to-transparent xl:p-16 sm:p-8 p-4 flex items-center justify-between">
         <img className="h-full sm:mr-4 mr-0" src={img1} alt="img1" />
 
         <p className="sm:w-[70%] sm:mt-0 mt-4 w-full text-2xl text-white font-light">
@@ -29,7 +29,7 @@ const Whyneed = () => {
             <MdOutlineArrowOutward />
           </span>
         </p>
-      </div>
+      </div> */}
 
       <div className="sm:w-5/6 w-[98%] md:w-[98%] xl:p-16 sm:p-8 p-4 sm:h-96 container mx-auto mt-10 backdrop-blur-lg  bg-gradient-to-b from-gray-100/20 to-transparent flex flex-row-reverse items-center justify-between">
 
